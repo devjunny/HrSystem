3dd5e401304508f3a32dbc2d897a9d736f27104c
Reuben McNinson <successfulreuben@gmail.com>
2023-01-19 16:31:39 +0000

    Add project files.

---
+++ HrSystem/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HrSystem.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //CORS CONFIGURATIONS
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options => {
+            options.AddPolicy("CorsPolicy", builder => 
+               builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { 
+            
+        });
+    }
+}
